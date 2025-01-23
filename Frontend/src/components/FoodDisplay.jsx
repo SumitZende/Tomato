75a8938ecb0c00677a1eca4ca7fb4847776c68df
Sumit_Zende <sumitzende652@gmail.com>
2025-01-23 17:38:20 +0530

    add filter for ExploredMenu as per category

--- Frontend/src/components/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay.jsx
@@ -1,27 +1,32 @@
-import React, { useContext } from 'react'
-import { StoreContext } from '../context/storecontext'
-import FoodItem from './FoodItem'
+import { useContext } from "react";
+import { StoreContext } from "../context/StoreContext.jsx";
+import FoodItem from "./FoodItem";
 
-export default function FoodDisplay() {
-
-    const {food_list} =useContext(StoreContext)
+export default function FoodDisplay({ category }) {
+  const { food_list } = useContext(StoreContext);
 
   return (
-    <div className='mt-[30px]' id='food-dislpay'>
-        <h2 className='text-[max(2vw,24px)] font-medium'> Top dishese near you</h2>
-        <div className="grid grid-cols-auto-fill-240 mt-[30px] gap-[30px] gap-y-[50px]">
-            {food_list.map((item,index)=>{
-             return <FoodItem key={index}
-                              id={item._id}
-                              name={item.name}
-                              description={item.description}
-                              price={item.price}
-                              image={item.image}/>
-              
-                
-            })
-            }
-        </div>
+    <div className="mt-[30px]" id="food-dislpay">
+      <h2 className="text-[max(2vw,24px)] font-medium">
+        {" "}
+        Top dishese near you
+      </h2>
+      <div className="grid grid-cols-auto-fill-240 mt-[30px] gap-[30px] gap-y-[50px]">
+        {food_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+        })}
+      </div>
     </div>
-  )
+  );
 }
