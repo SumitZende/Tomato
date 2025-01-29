b0a6577889c1b8e23948f72e9968294dfe27c1ae
Sumit_Zende <sumitzende652@gmail.com>
2025-01-29 17:51:32 +0530

    added navigation

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -6,9 +6,11 @@ export default function Navbar({setShowlogin}) {
     const [menu,setMenu]=useState("menu");
   return (
     <div className="py-5 px-0 flex justify-between items-center ">
+      <Link to={'/'}>
       <img src={assets.logo} alt="logo" 
           className="w-40 
                     max-lg:w-[140px] max-md:w-[120px]" />
+      </Link>
       <ul className="flex gap-[20px] list-none text-navbar_font text-[18px] cursor-pointer  
                     max-lg:gap-5 max-lg:text-lg max-md:gap-3 max-md:text-base max-sm:hidden">
         <Link to={'/'} onClick={()=>setMenu("home")} className={menu==="home"?"pb-[2px] border-b-2 border-b-navbar_font":""}>home</Link>
@@ -19,10 +21,12 @@ export default function Navbar({setShowlogin}) {
       <div className="flex items-center gap-[40px]
                       max-lg:gap-7 max-md:gap-5">
         <img src={assets.search_icon} alt="search-icon" className="max-lg:w-6 max-md:w-5" />
-        <div className="relative max-lg:w-6">
+        <div className="relative max-lg:w-6 cursor-pointer">
+          <Link to="/cart">
             <img src={assets.basket_icon} alt="" 
             className="max-lg:w-6 max-md:w-5"  />
             <div className="absolute min-w-[10px] min-h-[10px] bg-tomato rounded-[5px] top-[-8px] right-[-8px]"></div> 
+          </Link>
         </div>
         <button 
             className="bg-transparent text-[16px] text-navbar_font border border-tomato 
