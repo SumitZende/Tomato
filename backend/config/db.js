cdd46af313b2398758e5eb3a1f63bf4205d52231
Sumit_Zende <sumitzende652@gmail.com>
2025-02-06 15:10:39 +0530

    env variable setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 import multer from "multer";
-
+import dotenv from "dotenv";
+dotenv.config();
 
 //databse  connection Config
-export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://Tomato:kSzv74J1ut65hMgd@cluster0.tyrdc.mongodb.net/tomato')
-    .then(()=>console.log("DB Connect"));
-    
-}
-
+export const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGODB_URL, {})
+    .then(() => console.log("DB Connect"));
+};
 
 // Image Storage engine
 const storage = multer.diskStorage({
-    destination:"uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
-    }
-})
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
 
-export const upload=multer({storage:storage});
+export const upload = multer({ storage: storage });
